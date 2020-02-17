a5f9023ab9be637e3c7c99c9f78ff1f43d8594a7
Kamille Longalong <k.pagibigan@gmail.com>
2020-02-17 07:56:38 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.count == 1
+    array.join
+  elsif array.count == 2
+    array.join(" and ")
+  else 
+    a = array.pop 
+    array.join(", ") + ", and " + a
+  end
 end
